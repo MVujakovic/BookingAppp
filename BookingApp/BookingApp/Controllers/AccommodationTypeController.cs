5fa1e4e15cd2f99c82e62c775e2a5cf9bfd58815
MVujakovic <miljana_lo@hotmail.com>
2017-06-06 13:00:30 +0200

    Ostali kontroleri

---
+++ BookingApp/BookingApp/Controllers/AccommodationTypeController.cs
@@ -0,0 +1,121 @@
+﻿using BookingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace BookingApp.Controllers
+{
+    public class AccommodationTypeController : ApiController
+    {
+        private BAContext db = new BAContext();
+
+        [HttpGet]
+        [Route("AccomodationTypes")]
+        public IQueryable<AccommodationType> m1()
+        {
+            return db.AccomodationTypes;
+        }
+
+        [HttpGet]
+        [Route("AccomodationType/{id}")]
+        public IHttpActionResult m2(int id)
+        {
+            AccommodationType at = db.AccomodationTypes.Find(id);
+            if (at == null)
+            {
+                return NotFound();
+            }
+            return Ok(at);
+        }
+
+        [HttpPut]
+        [Route("AccomodationType/{id}")]
+        public IHttpActionResult m2(int id, AccommodationType at)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != at.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(at).State = System.Data.Entity.EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                if (!AccommTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [HttpPost]
+        [Route("AccomodationType")]
+        [ResponseType(typeof(AccommodationType))]
+        public IHttpActionResult m4(AccommodationType at)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.AccomodationTypes.Add(at);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = at.Id }, at);
+        }
+
+        [HttpDelete]
+        [Route("AccommodationType/{id}")]
+        [ResponseType(typeof(AccommodationType))]
+        public IHttpActionResult m5(int id)
+        {
+            AccommodationType at = db.AccomodationTypes.Find(id);
+            if (at == null)
+            {
+                return NotFound();
+            }
+
+            db.AccomodationTypes.Remove(at);
+            db.SaveChanges();
+
+            return Ok(at);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private bool AccommTypeExists(int id)
+        {
+            return db.AccomodationTypes.Count(e => e.Id == id) > 0;
+        }
+
+    }
+}
